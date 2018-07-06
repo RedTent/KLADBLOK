a512470be89db9dead3b38abe837de1e6e82fc21
Johan van Tent <tentvanjohan@hotmail.com>
2018-07-06 23:57:50 +0200

    eerste stappen voor opschonen twn en opbouwen hierarchie

---
+++ TWN_hierarchical.R
@@ -0,0 +1,48 @@
+library(dplyr)
+
+get_twn <- function(){
+require(readxl)
+  
+url <- "http://sofus.ecosys.nl/Taxus/Downloads/Taxalists/TWNList.XLS"
+destfile <- "twn.xls"
+curl::curl_download(url, destfile)
+twn <- suppressMessages(read_excel(destfile, col_types = c("text", "text", "text", "text", "text", "text", 
+                                          "text", "text", "text", "text", "date", "text")))
+twn
+}
+
+twn <- get_twn()
+
+# twee loops omdat sommige een dubbele doorverwijzing hebben
+twn_preferred <- twn %>% mutate(taxonname_preferred = if_else(is.na(refername),taxonname, refername)) %>% 
+    rename(taxonname_orig = taxonname) %>% 
+    select(taxonname_preferred, taxontype, taxonname_orig) %>%  
+    left_join(twn, by = c("taxonname_preferred" = "taxonname", "taxontype" = "taxontype")) %>% 
+    mutate(taxonname_preferred = if_else(is.na(refername), taxonname_preferred, refername)) %>% 
+    select(taxonname_preferred, taxontype, taxonname_orig) %>%  
+    left_join(twn, by = c("taxonname_preferred" = "taxonname", "taxontype" = "taxontype")) %>% 
+    select(taxontype, taxonname_orig, taxonname_preferred, localname, taxongroup, taxonlevel, parentname, date, status)
+    
+#twn %>% select(taxonlevel) %>% distinct() %>% arrange(taxonlevel)
+
+twn_levels <- twn_preferred %>% select(taxontype, taxonname_preferred, taxonlevel)
+
+twn_parents <- twn_preferred %>% 
+  select(taxontype, taxonname_preferred, taxonlevel, parentname) %>% 
+  left_join(twn_levels, by = c("parentname" = "taxonname_preferred", "taxontype" = "taxontype")) %>% 
+  rename(parentlevel = taxonlevel.y, taxonlevel = taxonlevel.x)
+  
+
+
+# TWN onvolkomenheden -----------------------------------------------------
+
+dubbele_doorverwijzing <- twn %>% mutate(taxonname_preferred = if_else(is.na(refername),taxonname, refername)) %>% 
+  select(taxonname_preferred, taxontype, taxonname) %>%  
+  left_join(twn, by = c("taxonname_preferred" = "taxonname", "taxontype" = "taxontype")) %>% 
+  filter(status == "20")  
+
+missende_verwijzing <- twn %>% filter(is.na(refername), status == "20") 
+
+missende_parents <- twn %>%  filter(is.na(parentname)) #%>% group_by(taxonlevel) %>% summarise(n=n()) %>% select(n) %>% sum()
+
+missende_parents_preferred <- twn_preferred %>%  filter(is.na(parentname)) #%>% group_by(status,taxonlevel) %>% summarise(n=n()) %>% as.data.frame() 
