796c32565f425e3f3d7c4cf5fb2c795ddf42f9fb
Johan van Tent <tentvanjohan@hotmail.com>
2018-07-17 21:14:50 +0200

    simplified code

--- stat_packcircles.R
+++ stat_packcircles.R
@@ -1,36 +1,31 @@
 library(ggplot2)
-
-compute_circle_groups <- function(data, scales, ...){
-  require(packcircles)
-  
-  data$id <- row(data)[,1]
-  positions <- circleProgressiveLayout(data$y)
-  circles <- circleLayoutVertices(positions, npoints = 100)
-  #colnames(circles)[[3]] <- "group"
-  #print(data)
-  #print(circles)
-  data$y <- NULL
-  data$group <- NULL
-  data <- merge(circles, data, by = "id", sort = FALSE)
-  #print(data)
-  names(data)[names(data) == "id"] <- "group"
-  data
-}
-
+library(packcircles)
 
 StatPackCircles <- ggproto("StatPackCircles", Stat,
                            
-                           compute_layer = compute_circle_groups,
-                           required_aes = c("y", "group")
+                           compute_layer = function(data, scales, ...){
+                             data$id <- row(data)[,1]
+                             circles <- packcircles::circleLayoutVertices(packcircles::circleProgressiveLayout(data$y), 
+                                                                          npoints = 100)
+                             data$y <- NULL
+                             data$group <- NULL
+                             data <- merge(circles, data, by = "id", sort = FALSE)
+                             names(data)[names(data) == "id"] <- "group"
+                             data
+                           },
+                           
+                           required_aes = c("y")
 )
 
 stat_packcircles <- function(mapping = NULL, data = NULL, geom = "polygon",
                        position = "identity", na.rm = FALSE, show.legend = NA, 
                        inherit.aes = TRUE, ...) {
   
-  layer(
+  ggplot2::layer(
     stat = StatPackCircles, data = data, mapping = mapping, geom = geom, 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
+
+randomize <- function(data){data[sample(nrow(data)),]}
