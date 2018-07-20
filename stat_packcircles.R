03cc95da7a3c806716a962b15ea716ab1108d7f0
Johan van Tent <tentvanjohan@hotmail.com>
2018-07-20 08:55:04 +0200

    added stat_packcircles_label for labelling packed circles with geom_text or geom_text_repel or geom_label

--- stat_packcircles.R
+++ stat_packcircles.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(packcircles)
+library(dplyr)
+library(ggrepel)
 
 StatPackCircles <- ggproto("StatPackCircles", Stat,
                            
@@ -29,3 +31,33 @@ stat_packcircles <- function(mapping = NULL, data = NULL, geom = "polygon",
 }
 
 randomize <- function(data){data[sample(nrow(data)),]}
+
+
+StatPackcirclesLabel <- ggproto("StatPackcirclesLabel", Stat,
+                                compute_layer = function(data, scales, ...){
+                                  #print(data)
+                                  #data$id <- row(data)[,1]
+                                  circle_centers <- packcircles::circleProgressiveLayout(data$y)
+                                  data$y <- NULL
+                                  data <- cbind(circle_centers,data)
+                                  #data$group <- NULL
+                                  #data <- merge(circles, data, by = "id", sort = FALSE)
+                                  #names(data)[names(data) == "id"] <- "group"
+                                  print(data)
+                                  data
+                                  
+                                }
+                                
+                                
+                                                                )
+
+stat_packcircles_label <- function(mapping = NULL, data = NULL, geom = "text",
+                             position = "identity", na.rm = FALSE, show.legend = NA, 
+                             inherit.aes = TRUE, ...) {
+  
+  ggplot2::layer(
+    stat = StatPackcirclesLabel, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
