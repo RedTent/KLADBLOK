ea5851b39a6959ed46da7d4257b3c3c00f13e015
Johan van Tent <tentvanjohan@hotmail.com>
2018-12-30 11:43:11 +0100

    geinstalleerde packages 3.5

---
+++ geinstalleerde_packages_3.5.R
@@ -0,0 +1,44 @@
+install.packages(c(# basics
+                   "tidyverse",
+                   "openxlsx",
+                   
+                   # Statistics
+                   "modelr",
+                   "caret",
+                   "e1071",
+                   "glmnet",
+                   "tree",
+                   "vegan",
+                   "randomForest",
+                   "leaps",
+                   "gbm",
+                   "ISLR",
+                   "MASS",
+                   "zyp",
+                   "Kendall",
+                   "trend",
+                   
+                   # GIS
+                   "rgdal",
+                   "gstat",
+                   
+                   # Visualisation
+                   "hexbin",
+                   "ggrepel",
+                   "gridExtra",
+                   "shiny",
+                   "htmlwidgets",
+                   "leaflet",
+                   "plotly",
+                   "packcircles",
+                   "ggmap",
+                   "DT",
+                   
+                   "microbenchmark",
+                   "devtools",
+                   "roxygen2",
+                   "testthat",
+                   "curl",
+
+                   "swirl"))
+devtools::install_github("RedTent/JT")
